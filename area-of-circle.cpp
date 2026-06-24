7ff231d4c5c71cb9b9b99e328468aea8f052bb40
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-24 18:46:07 +0530

    Add area of circle

---
+++ area-of-circle.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float radius;
+    cin>> radius;
+    float area = 3.141592 * radius * radius;
+    cout << "The area of the circle is: " << area << endl;
+}

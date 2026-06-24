a16d16bb174fe06ce5299a2a75aecb68beb7f9ad
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-24 19:30:44 +0530

    Add square a number

---
+++ square-number.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<< "Enter the number to be squared: ";
+    float num ;
+    cin>>num ;
+    float square = num * num;
+    cout<< "The square of " << num << " is: " << square << endl;
+}

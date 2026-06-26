bd60d022bfc0fb72ecef6bfcd96afc0a35bc971b
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-26 19:43:16 +0530

    Add program to test parity of an integer

---
+++ parity.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    if (x%2 == 0) cout<<"Number is Even";
+    else cout<<"Number is Odd";
+}

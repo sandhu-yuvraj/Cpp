3ad54dfa3e9f987d9a8ea2eef63149314e410555
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-26 21:36:34 +0530

    Add software to calculate profit and loss

---
+++ profit-loss.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int c, s;
+    cout << "Enter the cost price of the item: ";
+    cin>> c;
+    cout << "Enter the selling price of the item: ";
+    cin>> s;
+    if (s > c) cout<< "Profit: " << s - c;
+    else if (c > s) cout<< "Loss: " << c - s;
+    else cout<< "No Profit No Loss";
+}

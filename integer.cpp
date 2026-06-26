f07a324faba20d0863e848843c10fffe61c9409d
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-06-26 19:42:09 +0530

    Add program to test whether entered number is an integer

---
+++ integer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    float x;
+    cout << "Enter a number: ";
+    cin >> x;
+    int y = (int)x;
+    if (x == y) 
+    {
+        cout << "The number is an integer." << endl;
+    } else 
+    {
+        cout << "The number is not an integer." << endl;
+    }
+}

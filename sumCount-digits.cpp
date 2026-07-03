69236b0ede198dfc57ea24e1fb9d377a9669543d
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-03 09:28:00 +0530

    Add program

---
+++ sumCount-digits.cpp
@@ -0,0 +1,18 @@
+// Takes integral number as input and adds and counts its digits
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter a number: ";
+    cin>>number;
+    int sum = 0;
+    int digit = 0;
+    while (number != 0)
+    {
+        sum = sum + number%10;
+        number = number/10 ;
+        digit++ ;
+    }
+    cout<<"Number of digits: "<<digit<<endl;
+    cout<<"Sum of digits: "<<sum<<endl;
+}

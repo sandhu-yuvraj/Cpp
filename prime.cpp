71ff671df1e1b00b014273a0ae4a6cfd28f08028
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-04 11:58:37 +0530

    Add prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,24 @@
+/*Checks if a number is prime or composite
+  Prime number: Number is which is only divisble by 1 or itself
+  Composite number: Number which has multiple(s) other than 1 and itself
+  Negative integers, 0 and 1 can not be classfied as prime or composite*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if (n<=1) cout<<"Neither Prime nor Composite";
+   else
+   {
+        bool Prime= true;
+         for(int i = 2 ; i<=n-1; i++)
+         {
+            if (n%i == 0)  {Prime = false; break;}
+         } 
+                                    if (Prime) cout<<"Prime";
+                                    else cout<<"Composite";
+    }
+        return 0;
+}

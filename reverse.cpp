76f6e0229ade318e74bb5f9b196ef259795cd8b7
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-06 16:36:17 +0530

    Add program to reverse a number

---
+++ reverse.cpp
@@ -0,0 +1,18 @@
+// Takes a number as input
+// Reverses it
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if (n==0) cout<<0;
+    else{
+        int rev=0;
+        while(n!=0){
+            rev= rev*10+n%10; // First multiply by 10 then add the last digit
+            n/=10;            // Keep removing the last digit
+        }
+        cout<<"Reversed number is: "<<rev;
+    }
+}

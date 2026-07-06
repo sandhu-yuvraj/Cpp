1f4289be3c0d390f3a1e901668d052576e423615
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-06 17:58:48 +0530

    Update to display their sum

--- reverse.cpp
+++ reverse.cpp
@@ -6,6 +6,7 @@ int main (){
     int n;
     cout<<"Enter a number: ";
     cin>>n;
+    int old = n;  // Since we will be changing n, therefore we store the original number in old
     if (n==0) cout<<"Reversed number is: "<<0;
     else{
         int rev=0;
@@ -13,6 +14,8 @@ int main (){
             rev= rev*10+n%10; // First multiply by 10 then add the last digit
             n/=10;            // Keep removing the last digit
         }
-        cout<<"Reversed number is: "<<rev;
+        cout<<"Reversed number is: "<<rev<<endl;
+        cout<<"Old number was: "<<old<<endl;
+        cout<<"Sum of old and reversed number is: "<<old+rev<<endl;
     }
 }

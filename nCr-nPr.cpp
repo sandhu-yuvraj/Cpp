76a657d3befb1f2d6a2ad27f9f430b5693b87cd2
Yuvraj Sandhu <yuvrajsinghsandhu2008@gmail.com>
2026-07-18 11:41:55 +0530

    Add nCr-nPr.cpp

---
+++ nCr-nPr.cpp
@@ -0,0 +1,27 @@
+/* nCr and nPr are mathematical functions used in combinatorics to calculate combinations and permutations, respectively.
+   These functions are useful in various fields such as probability, statistics, and computer science.
+   These functions can be implemented in C++ using recursive or iterative methods to calculate factorials and then use those factorials to compute nCr and nPr values.
+   Using functions, we can calculate nCr and nPr,
+   where nCr = n! / (r! * (n - r)!), and nPr = n! / (n - r )! */
+
+#include <bits/stdc++.h>  // Non standard library which includes most of the standard C++ libraries
+using namespace std;
+int fact(int x){
+    int fact = 1;
+    for (int i = 1; i <= x; i++){
+        fact *= i;
+    }
+    return fact;
+}
+int main (){
+    int n, r;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<"Enter r: ";
+    cin>>r;
+    int c = fact(n)/(fact(r) * fact(n-r)) ;
+    int p = fact(n)/fact(n-r);
+    cout<<"nCr: "<<c<<endl;
+    cout<<"nPr: "<<p;
+    return 0;
+}
